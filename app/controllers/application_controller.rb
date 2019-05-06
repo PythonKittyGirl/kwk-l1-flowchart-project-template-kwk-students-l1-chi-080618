c8cb1f24e2d7f3b9f69a38f50ea69d4d05ec775e
PythonKittyGirl <ung.deborah@gmail.com>
2019-05-06 21:17:32 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,37 @@
+require './config/environment'
+require './app/models/quiz.rb'
+
 class ApplicationController < Sinatra::Base
 
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
   end
+  
+  get '/' do
+    return erb :index
+  end
 
+  post '/results' do
+
+  points_array = params.values 
+  
+    total = 0
+    points_array.each do |point| 
+      total += point.to_i 
+    end
+
+    result = calculate_result(total) 
+    
+    
+    if result == "jiaming" 
+      return erb :jiaming
+    elsif result == "taylor"
+      return erb :taylor
+    elsif result == "isabel"
+      return erb :isabel
+    end
+    
+    
+  end
 end
---
+++ project.rb
@@ -0,0 +1,5 @@
+def user_answer
+  puts "What do you like better reading or playing sports?"
+ user_answer = gets.chomp
+  if user_answer = "playing sports"
+    puts "What do you like "
